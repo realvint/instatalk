38b9089ad611a44ddbf29e190c65ca0a73f4b12e
realvint <victor.malukhin@gmail.com>
2022-02-01 21:42:33 +0300

    implement messages

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,8 @@
 class Room < ApplicationRecord
   before_create :generate_token
 
+  has_many :messages
+
   def to_param
     token
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_182125) do
+ActiveRecord::Schema.define(version: 2022_02_01_183051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id"
+    t.bigint "room_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string "token"
@@ -27,4 +37,6 @@ ActiveRecord::Schema.define(version: 2022_02_01_182125) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
 end
