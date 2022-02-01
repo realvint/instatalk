7c7703e5bdadf199847f84270d81545bf78debbd
realvint <victor.malukhin@gmail.com>
2022-02-01 21:30:22 +0300

    implement rooms

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_181212) do
+ActiveRecord::Schema.define(version: 2022_02_01_182125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "token"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "nickname"
